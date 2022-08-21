d3ea69ec3bf876ef098be5fe30bff0a40665275b
Salah <ahoraian@gmail.com>
2022-08-21 09:50:36 +0000

    initial commit

---
+++ src/http/http_access_token.go
@@ -0,0 +1,50 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	atDomain "github.com/salahfarzin/bookstore_oauth-api/src/domain/access_token"
+	"github.com/salahfarzin/bookstore_oauth-api/src/services/access_token"
+	"github.com/salahfarzin/bookstore_utils-go/rest_errors"
+)
+
+type AccessTokenHandler interface {
+	GetById(*gin.Context)
+	Create(*gin.Context)
+}
+
+type accessTokenHandler struct {
+	service access_token.Service
+}
+
+func NewAccessTokenHandler(service access_token.Service) AccessTokenHandler {
+	return &accessTokenHandler{
+		service: service,
+	}
+}
+
+func (handler *accessTokenHandler) GetById(c *gin.Context) {
+	accessToken, err := handler.service.GetById(c.Param("access_token_id"))
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+	c.JSON(http.StatusOK, accessToken)
+}
+
+func (handler *accessTokenHandler) Create(c *gin.Context) {
+	var request atDomain.AccessTokenRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		restErr := rest_errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status(), restErr)
+		return
+	}
+
+	accessToken, err := handler.service.Create(request)
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+	c.JSON(http.StatusCreated, accessToken)
+}
---
+++ src/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/salahfarzin/bookstore_oauth-api/src/app"
+)
+
+func main() {
+	app.StartApplication()
+}
